816f24ec046d37c35fc7827292034955e91bd4fa
Ting <ting@hashicorp.com>
2025-10-06 16:05:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,19 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_network" "private_net" {
+  name = "internal_network"
+  driver = "bridge"
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
@@ -18,7 +26,10 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.name
-  name  = "nginx"
+  name  = "nginx_server"
+  networks_advanced {
+    name = docker_network.private_net.name
+  }
   ports {
     internal = 80
     external = 8000
