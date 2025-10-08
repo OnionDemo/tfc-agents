bb7f5257f3a7a83029b72915b2bb9ffe36042b29
Ting <ting@hashicorp.com>
2025-10-08 11:17:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,22 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
-      version = "~> 3.0"
     }
   }
 }
 
 provider "docker" {}
 
-module "docker_app" {
-  source         = "./modules/docker-app"
-  container_name = var.container_name
-  image_name     = var.image_name
-  internal_port  = var.internal_port
-  external_port  = var.external_port
-  network_name   = var.network_name
-  volume_name    = var.volume_name
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.name
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
