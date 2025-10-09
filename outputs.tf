92bd637281bc0ff3d18129c0ce7ff807281b9b0b
Ting <ting@hashicorp.com>
2025-10-08 17:31:47 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,14 @@
-output "container_id" {
-  description = "ID of the created Docker container"
-  value       = docker_container.nginx.id
+output "container_name" {
+  description = "Name of the Docker container"
+  value       = var.container_name
 }
 
-output "image_name" {
-  description = "Docker image name used"
-  value       = docker_image.nginx.name
+output "image_used" {
+  description = "Docker image used"
+  value       = var.image_name
 }
 
+output "docker_network_used" {
+  description = "Docker network attached (empty if host network)"
+  value       = var.docker_network
+}
